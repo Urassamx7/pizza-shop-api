2d41f18472682b6513222ecbf3dcad8295bfa517
Marvin Mussacate <marvin.lagas01234@gmail.com>
2025-09-08 07:00:41 +0200

    drizzle relations

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,12 +1,27 @@
 import { text, pgTable, timestamp } from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
+import { users } from "./users";
+import { relations } from "drizzle-orm";
 
 
 export const restaurant = pgTable("restaurants", {
     id: text("id").$defaultFn(() => createId()).primaryKey(),
+    managerId: text('manager_id').references(() => users.id, {
+        onDelete: "set null",
+    }),
     name: text("name").notNull(),
     description: text("description"),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 
 });
+
+export const restaurantsRelations = relations(restaurant, ({ one }) => {
+    return {
+        manager: one(users, {
+            fields: [restaurant.managerId],
+            references: [users.id],
+            relationName: "restaurant_manager"
+        })
+    }
+})
