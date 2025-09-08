6e4f67a4c5930061a03a311b483dc47c9c6d3cea
Marvin Mussacate <marvin.lagas01234@gmail.com>
2025-09-08 10:10:32 +0200

    create auth link

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.url().min(1),
+  API_BASE_URL: z.url().min(1),
+  AUTH_REDIRECT_URL: z.url().min(1),
 })
 
 export const env = envSchema.parse(process.env)
