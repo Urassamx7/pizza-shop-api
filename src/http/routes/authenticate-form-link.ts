d4c253640ddf941b3ceda18b9920ad7a249ce569
Marvin Mussacate <marvin.lagas01234@gmail.com>
2025-09-08 15:03:26 +0200

    route: signout

--- src/http/routes/authenticate-form-link.ts
+++ src/http/routes/authenticate-form-link.ts
@@ -7,7 +7,7 @@ import { auth } from './auth'
 
 export const authenticateFromLink = new Elysia()
   .use(auth)
-  .get('/auth-links/authenticate', async ({ query, jwt, set, setCookie }) => {
+  .get('/auth-links/authenticate', async ({ query, set, signUser }) => {
     const { code, redirect } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -33,15 +33,9 @@ export const authenticateFromLink = new Elysia()
       where: eq(restaurant.managerId, authLinkFromCode.userId),
     })
 
-    const token = await jwt.sign({
+    await signUser({
       sub: authLinkFromCode.userId,
       restaurantId: managedRestaurant?.id,
-    })
-
-    setCookie('auth', token, {
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      path: '/',
     })
 
     await db.delete(authLinks).where(eq(authLinks.code, code as string))
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import { Elysia } from 'elysia'
 import { sendAuthLink } from './routes/auth-link'
 import { authenticateFromLink } from './routes/authenticate-form-link'
 import { registerRestaurant } from './routes/register-restaurant'
+import { signOut } from './routes/sign-out'
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(signOut)
 
 app.listen(3333, () => {
   console.log('🔥 Http server running!')
