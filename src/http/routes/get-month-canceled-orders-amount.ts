b70079eac697fd5462b12625ffec114dac48fa40
Marvin Mussacate <marvin.lagas01234@gmail.com>
2025-09-11 16:31:02 +0200

    route: get month canceled orders

---
+++ src/http/routes/get-month-canceled-orders-amount.ts
@@ -0,0 +1,58 @@
+import dayjs from 'dayjs'
+import Elysia from 'elysia'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import { auth } from './auth'
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import { eq, and, gte, sql, count } from 'drizzle-orm'
+
+export const getMonthCanceledOrdersAmount = new Elysia()
+  .use(auth)
+  .get('/metrics/month-canceled-orders-amount', async ({ getCurrentUser }) => {
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    const today = dayjs()
+    const lastMonth = today.subtract(1, 'month')
+    const startOfLastMonth = lastMonth.startOf('month')
+
+    const ordersPerMonth = await db
+      .select({
+        monthWithYear: sql<string>`TO_CHAR(${orders.createdAt},'YYYY-MM')`,
+        amount: count(),
+      })
+      .from(orders)
+      .where(
+        and(
+          eq(orders.restaurantId, restaurantId),
+          eq(orders.status, 'canceled'),
+          gte(orders.createdAt, startOfLastMonth.toDate()),
+        ),
+      )
+      .groupBy(sql`TO_CHAR(${orders.createdAt},'YYYY-MM')`)
+
+    const currentMonthWithYear = today.format('YYYY-MM') // 2025-09
+    const lastMonthWithYear = lastMonth.format('YYYY-MM') // 2025-08
+
+    const currentMonthOrdersAmount = ordersPerMonth.find((orderPerMonth) => {
+      return orderPerMonth.monthWithYear === currentMonthWithYear
+    })
+
+    const lastMonthOrdersAmount = ordersPerMonth.find((lastOrderPerMonth) => {
+      return lastOrderPerMonth.monthWithYear === lastMonthWithYear
+    })
+
+    const diffFromLastMonth =
+      currentMonthOrdersAmount && lastMonthOrdersAmount
+        ? (currentMonthOrdersAmount.amount * 100) / lastMonthOrdersAmount.amount
+        : null
+    return {
+      Amount: currentMonthOrdersAmount?.amount,
+      diffFromLastMonth: diffFromLastMonth
+        ? Number((diffFromLastMonth - 100).toFixed(2))
+        : 0,
+    }
+  })
